8553807ec18e7575e27fe2bb6611ae24580edc57
Mugamba Bruno <brunohectre@gmail.com>
2023-10-16 13:49:11 +0300

    modified business model

--- src/controllers/business.controller.js
+++ src/controllers/business.controller.js
@@ -1,9 +1,9 @@
 const businessModel = require("../models/business.model")
 class BusinessController {
-    static async getAll(req, res) {
+    static async getAll(req, res)  {
         try {
-            let businesses = Business.find();
-            res.status(200).json({ data: businesses });
+            let business = await businessModel.find();
+            res.status(200).json({ data: business });
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
