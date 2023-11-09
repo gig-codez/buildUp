3d61ed3a742d230f029e7d89eadb320647ff5ef0
Muhangi2 <eliodamutiba@gmail.com>
2023-11-09 17:04:32 +0300

    freelancermodel

--- src/models/freelancer.model.js
+++ src/models/freelancer.model.js
@@ -56,6 +56,9 @@ const freelancerSchema = new mongoose.Schema(
       code: String,
       expiry: Date,
     },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
   },
   {
     timestamps: true,
