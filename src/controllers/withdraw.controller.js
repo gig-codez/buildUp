435aac3059249ace1d034fc232b883dffbdaf722
Mugamba Bruno <brunohectre@gmail.com>
2024-06-25 18:37:42 +0300

    added pagination on withdraws

--- src/controllers/withdraw.controller.js
+++ src/controllers/withdraw.controller.js
@@ -3,8 +3,31 @@ module.exports = class WithdrawController {
     // get withdraws
     static async getWithdraws(req, res) {
         try {
-            const withdraws = await withdrawModel.find().populate('contractor').populate('supplier').sort({ _id: -1 });
-            res.status(200).json(withdraws);
+            // ADDING PAGINATION FUNCTIONALITY
+            const page = parseInt(req.query.page) || 1; // Default to page 1 if page query param is not provided
+            const pageSize = parseInt(req.query.pageSize) || 10; // Default page size to 10 if pageSize query param is not provided
+
+            const totalDocuments = await withdrawModel
+                .find()
+                .countDocuments();
+            const totalPages = Math.ceil(totalDocuments / pageSize);
+
+            // Calculate the number of documents to skip
+            const skipDocuments = (page - 1) * pageSize;
+            const withdraws = await withdrawModel.find()
+                .populate('contractor')
+                .populate('supplier')
+                .skip(skipDocuments)
+                .limit(pageSize)
+                .sort({ _id: -1 });
+            //    response
+            res.status(200).json({
+                totalDocuments,
+                totalPages,
+                currentPage: page,
+                pageSize,
+                withdraws,
+            });
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
