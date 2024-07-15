401ea421eced5e639915bab9a22e5b77be11e7ec
Mugamba Bruno <brunohectre@gmail.com>
2024-07-15 23:42:25 +0300

    modified server

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const {
   connAcceptedArea,
 } = require("./global");
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
