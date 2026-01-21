7a642b162b18ac7dd78d8bea4e606b2cb3ad2f8c
bruno <brunohectre@gmail.com>
2026-01-21 09:48:19 +0300

    Add logging for email sending process and error handling in mailSender

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const mailSender = async (email, title, body) => {
+  console.log("Sending mail to:", email, " by:", process.env.MAIL_FROM, " with token :", process.env.RESEND_API_KEY);
   try {
     const { data, error } = await resend.emails.send({
       from: `BuildUp Uganda <${process.env.MAIL_FROM}>`,
@@ -18,6 +19,7 @@ const mailSender = async (email, title, body) => {
 
     return data;
   } catch (error) {
+    console.log("Mail sending error:", error);
     throw new Error(error);
   }
 };
