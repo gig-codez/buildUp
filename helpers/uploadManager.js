a92011b8d8f1d5cb7db9889e8327ded62cba1c64
andrua-joshua <andruajoshua096@gmail.com>
2026-05-26 17:24:58 +0300

    ....updates

--- helpers/uploadManager.js
+++ helpers/uploadManager.js
@@ -1,12 +1,14 @@
 const multer = require("multer");
 const firebase = require("firebase-admin");
 const serviceAccount = require("./build-up-deb9a-firebase-adminsdk-n0d0y-a15cf778b8.json");
-// Initialize Firebase
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://build-up-deb9a-default-rtdb.firebaseio.com",
-  storageBucket: "build-up-deb9a.appspot.com",
-});
+// Initialize Firebase (guard against double-init on hot reloads)
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://build-up-deb9a-default-rtdb.firebaseio.com",
+    storageBucket: "build-up-deb9a.appspot.com",
+  });
+}
 const uploaderManager = () => {
   return multer({
     storage: multer.memoryStorage(),
